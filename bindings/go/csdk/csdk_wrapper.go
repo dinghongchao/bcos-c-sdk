3330dff9c85b138923c38efcbb4ab6795f404e0f
dhc <dhc@zowo.art>
2022-10-21 15:34:59 +0800

    remove unused import

--- bindings/go/csdk/csdk_wrapper.go
+++ bindings/go/csdk/csdk_wrapper.go
@@ -81,5 +81,7 @@ func CreateSignedTransaction(groupId string, chainId string, to string, data str
 	}
 
 	C.bcos_sdk_destroy_keypair(key_pair)
-	return nil, C.GoString(&signed_tx)
+	p := signed_tx.getString()
+	defer C.free(unsafe.Pointer(p))
+	return nil, C.GoString(p)
 }
